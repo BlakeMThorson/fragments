04fa7ec9b8c0d109d087da985d7397ca2af76b37
Blake Thorson <51890547+BlakeMThorson@users.noreply.github.com>
2023-07-15 06:05:52 -0500

    Update fireball.lua

--- fireball.lua
+++ fireball.lua
@@ -1,3 +1,34 @@
-player = getObjectFromGUID("7f82e1")
+local player = getObjectFromGUID("2cb0e2")
+local AccelerationSpeed = .2
 
---literally just move forward
+function getGlobalForce(vector)
+  local local_pos = self:positionToWorld(vector)
+  local my_pos = self:getPosition()
+  local new_force = {x=local_pos.x - my_pos.x, y=local_pos.y - my_pos.y, z=local_pos.z - my_pos.z}
+  return new_force
+end
+
+--rotate to the player's rotation
+self.setRotation(player.getRotation())
+
+--teleport to the player
+local yRotRadians = math.rad(player.getRotation()[2]) -- getRotation instead of getPosition
+local direction = { x = math.sin(yRotRadians), z = math.cos(yRotRadians) }
+local spawnDistance = 25
+local cubeLocation = player.getPosition()
+local spawnLocation = { x = cubeLocation.x + direction.x * spawnDistance, y = cubeLocation.y , z = cubeLocation.z + direction.z * spawnDistance } -- direction.z is added for z calculation
+
+self.setPosition(spawnLocation)
+
+-- Move the fireball in the direction the player is facing
+local add_force = {x = direction.x * AccelerationSpeed, y = 0, z = direction.z * AccelerationSpeed} -- add force in the direction player is facing
+self:addForce(getGlobalForce(add_force), 2)
+
+-- Update function to keep moving the fireball
+function fixedUpdate()
+  to_check = self.getPosition()
+  if to_check.z > 33 || to_check.z < -36 || to_check.x < -41 || to_check.x > 4 then
+    self.destroyObject()
+  end
+  self:addForce(getGlobalForce(add_force), 2)
+end
